f660cd30ceec584973ce9feb0587f466cee36004
yyh <hope392163@163.com>
2022-08-03 18:52:18 +0800

    样式初始化

--- dist/index.js
+++ dist/index.js
@@ -6,19 +6,33 @@ var App = function App() {
       dbData = _React$useState2[0],
       setDbdata = _React$useState2[1];
 
+  var url = "http://bing.getlove.cn/bingImage";
   React.useEffect(function () {
     console.log(utools);
-    // utools.db.put({
-    //   _id: "demo",
-    //   data: "demo",
-    // });
-    setDbdata(utools.db.get("demo"));
   }, []);
 
   return React.createElement(
     "div",
-    { className: "text-4xl" },
-    JSON.stringify(dbData)
+    { className: "relative w-screen h-screen" },
+    React.createElement(
+      "div",
+      { className: "absolute top-0" },
+      React.createElement("img", { src: url })
+    ),
+    React.createElement(
+      "div",
+      { className: "filter blur-lg w-4/5 h-2/5 absolute top-0 bottom-0 left-0 right-0 m-auto flex flex-col justify-between items-center" },
+      React.createElement(
+        "div",
+        { className: "text-7xl" },
+        "pass out"
+      ),
+      React.createElement(
+        "div",
+        { className: "text-xl" },
+        "\u4ECA\u5929\u6211\u4EEC\u8981\u5B66\u7684\u8BCD\u662F pass out. To pass out \u610F\u601D\u662F\u5931\u53BB\u77E5\u89C9\u3002A pilot who allegedly passed out in the cockpit was arrested on suspicion of being drunk. \u4E00\u4E2A\u636E\u62A5\u9053\u5728\u9A7E\u9A76\u8231\u5185\u4E0D\u7701\u4EBA\u4E8B\u7684\u98DE\u884C\u5458\u56E0\u6D89\u5ACC\u9189\u9152\u88AB\u902E\u6355\u3002A Texas couple was arrested after they passed out in the car with drugs and their two young children inside. \u5F97\u514B\u8428\u65AF\u5DDE\u4E00\u7537\u4E00\u5973\u88AB\u8B66\u65B9\u902E\u6355\uFF0C\u53D1\u73B0\u4ED6\u4EEC\u65F6\uFF0C\u4ED6\u4EEC\u5DF2\u7ECF\u4E0D\u7701\u4EBA\u4E8B\uFF0C\u8F66\u91CC\u8FD8\u6709\u4ED6\u4EEC\u7684\u4E24\u4E2A\u5C0F\u5B69\u5B50\u548C\u6BD2\u54C1\u3002\u597D\u7684\uFF0C\u6211\u4EEC\u4ECA\u5929\u5B66\u4E60\u7684\u8BCD\u662F pass out, pass out, pass out\u2026"
+      )
+    )
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,30 @@
 const App = () => {
-  const [dbData, setDbdata] = React.useState({})
+  const [dbData, setDbdata] = React.useState({});
+
+  const url = `http://bing.getlove.cn/bingImage`;
   React.useEffect(() => {
     console.log(utools);
-    // utools.db.put({
-    //   _id: "demo",
-    //   data: "demo",
-    // });
-    setDbdata(utools.db.get("demo"))
   }, []);
 
-  return <div className="text-4xl">{JSON.stringify(dbData)}</div>;
+  return (
+    <div className="relative w-screen h-screen">
+      <div className="absolute top-0">
+        <img src={url} />
+      </div>
+      <div className="filter blur-lg w-4/5 h-2/5 absolute top-0 bottom-0 left-0 right-0 m-auto flex flex-col justify-between items-center">
+        <div className="text-7xl">pass out</div>
+        <div className="text-xl">
+          今天我们要学的词是 pass out. To pass out 意思是失去知觉。A pilot who
+          allegedly passed out in the cockpit was arrested on suspicion of being
+          drunk. 一个据报道在驾驶舱内不省人事的飞行员因涉嫌醉酒被逮捕。A Texas
+          couple was arrested after they passed out in the car with drugs and
+          their two young children inside.
+          得克萨斯州一男一女被警方逮捕，发现他们时，他们已经不省人事，车里还有他们的两个小孩子和毒品。好的，我们今天学习的词是
+          pass out, pass out, pass out…
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
