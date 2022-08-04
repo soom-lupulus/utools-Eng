9a3dc0ab5d30601626eea9978f770ab669637007
yyh <hope392163@163.com>
2022-08-04 18:16:22 +0800

    数据爬取代码完成

--- spider/getInfo.js
+++ spider/getInfo.js
@@ -5,13 +5,12 @@ const fs = require("fs");
 class Spider {
   index = 1050;
   word = "";
-  baseUrl =
-    `https://voa-story.com/%e5%ad%a6%e4%b8%aa%e8%af%8d-${this.index}-${this.word}/`;
+  baseUrl = `https://voa-story.com/%e5%ad%a6%e4%b8%aa%e8%af%8d`;
   arr = [];
 
   constructor() {
     this.instance = axios.create({
-      timeout: 10000,
+      timeout: 1000 * 30,
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
@@ -19,38 +18,59 @@ class Spider {
     });
   }
 
-  async grabData() {
+  async grabData(nameArr, i) {
+    const { num, content } = nameArr[i];
+    this.index = num;
+    this.word = content;
     try {
-      const res = await this.instance.get(this.baseUrl);
-      return res.data;
+      const res = await this.instance.get(`${this.baseUrl}-${this.index}`);
+      const article = this.parseHtml(res.data);
+      const item = {
+        index: num,
+        title: content,
+        article: article,
+      };
+      this.saveIt(`spider/data/data.json`, item);
+      console.log(`第${this.index}个单词${this.word}完成!`);
     } catch (error) {
-        console.log(error);
-        // fs.writeFileSync("spider/data/error.json", `第${this.index}页爬取失败\n`, { flag: 'a+' });
+      fs.writeFileSync(
+        "spider/data/error.json",
+        `第${this.index}个单词爬取失败\n`,
+        { flag: "a+" }
+      );
+      console.log(`第${this.index}个单词${this.word}失败!`);
+      await this.grabData(nameArr, this.index);
     }
   }
   parseHtml(html) {
-    if(!html) return;
+    if (!html) {
+      console.log("未获取到网页");
+      return;
+    }
     const $ = cheerio.load(html);
-    const string = $("article.small>p").text()
-    console.log(string);
+    const string = $("article.small>p").text();
+    return string;
   }
 
   saveIt(url, data) {
     try {
-      fs.writeFileSync(url, JSON.stringify(data));
+      fs.writeFileSync(url, JSON.stringify(data) + ",", { flag: "a+" });
     } catch (error) {
       console.log("保存出错了" + error);
     }
   }
 }
 
-const s = new Spider();
 
-const start = () => {
-  s.grabData().then((html) => {
-    s.parseHtml(html);
-  });
-};
 
-start()
-
+(async () => {
+  const s = new Spider();
+  const nameString = fs.readFileSync("spider/data/name.json").toString();
+  const nameArr = JSON.parse(nameString);
+  let i = nameArr.length - 1;
+  while (i >= 0) {
+    await s.grabData(nameArr, i)
+    --i
+  }
+  console.log("全部完成");
+})()
--- spider/index.js
+++ spider/index.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 class Spider {
-  index = 363;
+  index = 362;
   lastIndex = 363;
   baseUrl =
     "https://voa-story.com/category/%E5%AD%A6%E4%B8%AA%E8%AF%8D-learn-a-word/page/";
@@ -19,24 +19,37 @@ class Spider {
     });
   }
 
-  async grabData() {
+  async start() {
     try {
-      const res = await this.instance.get(this.baseUrl + this.index);
-      return res.data;
+      while (this.index < this.lastIndex) {
+        const res = await this.instance.get(this.baseUrl + this.index);
+        this.parseHtml(res.data);
+        console.log(`第${this.index}页完成`);
+        this.index++;
+      }
+      this.saveIt(`spider/data/1.json`, this.arr);
     } catch (error) {
-        console.log(error);
-        fs.writeFileSync("spider/data/error.json", `第${this.index}页爬取失败\n`, { flag: 'a+' });
+      fs.writeFileSync(
+        "spider/data/error.json",
+        `第${this.index}页爬取失败\n`,
+        { flag: "a+" }
+      );
+      // 错误重试
+      this.start()
     }
   }
   parseHtml(html) {
-    if(!html) return;
+    if (!html) return;
     const $ = cheerio.load(html);
     const self = this;
     $(".mg-posts-sec-inner article h4 a").each(function (index, element) {
-    //   console.log($(this).text());
+      //   console.log($(this).text());
       const text = $(this).text();
       const num = text.match(/[0-9]+/)[0];
-      const content = text.match(/[a-z]+/i)[0];
+      const content = text
+        .match(/[a-z\s’-]+/gi)
+        .join("")
+        .trim();
       const item = {
         num,
         content,
@@ -47,7 +60,7 @@ class Spider {
 
   saveIt(url, data) {
     try {
-      fs.writeFileSync(url, JSON.stringify(data));
+      fs.writeFileSync(url, JSON.stringify(data), { flag: "a+" });
     } catch (error) {
       console.log("保存出错了" + error);
     }
@@ -55,19 +68,20 @@ class Spider {
 }
 
 const s = new Spider();
+s.start();
 
-const start = () => {
-  s.grabData().then((html) => {
-    s.parseHtml(html);
-    console.log("第" + s.index + "页完成");
-    if (s.index < s.lastIndex) {
-      s.index++;
-      start();
-    } else {
-      s.saveIt(`spider/data/names${s.index}.json`, s.arr);
-      console.log("Ok");
-    }
-  });
-};
+// const start = () => {
+//   s.grabData().then((html) => {
+//     s.parseHtml(html);
+//     console.log("第" + s.index + "页完成");
+//     if (s.index < s.lastIndex) {
+//       s.index++;
+//       start();
+//     } else {
+//       s.saveIt(`spider/data/names${s.index}.json`, s.arr);
+//       console.log("Ok");
+//     }
+//   });
+// };
 
-start();
+// start();
