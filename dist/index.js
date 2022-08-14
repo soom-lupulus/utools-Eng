58b1de871237c912f8785722b4a52c5b4af391c6
yyh <hope392163@163.com>
2022-08-14 20:51:17 +0800

    数据获取成功

--- dist/index.js
+++ dist/index.js
@@ -4,15 +4,79 @@ import { DOCID } from "../config/index.js";
 var App = function App() {
   var _React$useState = React.useState({}),
       _React$useState2 = _slicedToArray(_React$useState, 2),
-      dbData = _React$useState2[0],
-      setDbdata = _React$useState2[1];
+      info = _React$useState2[0],
+      setInfo = _React$useState2[1];
 
   var _React$useState3 = React.useState(""),
       _React$useState4 = _slicedToArray(_React$useState3, 2),
       imgUrl = _React$useState4[0],
       setImgUrl = _React$useState4[1];
 
-  React.useEffect(function () {
+  var removeRevFromDBStorage = function removeRevFromDBStorage(DOCID) {
+    var _utools$db$remove = utools.db.remove(DOCID),
+        ok = _utools$db$remove.ok;
+
+    if (ok) {
+      utools.dbStorage.removeItem("eng_rev");
+      console.log("删除了");
+    }
+  };
+
+  var initData = function initData(doc_id, data) {
+    var doc = utools.db.get(doc_id);
+    // 新用户就新建数据库
+    if (!doc) {
+      // 创建数据库
+      var _utools$db$put = utools.db.put({
+        _id: doc_id,
+        data: data
+      }),
+          rev = _utools$db$put.rev,
+          ok = _utools$db$put.ok,
+          id = _utools$db$put.id;
+
+      if (ok) {
+        console.log("数据库创建成功, id为" + id);
+        // 存储rev
+        utools.dbStorage.setItem(doc_id + "_rev", rev);
+      } else {
+        console.log("数据库创建失败");
+      }
+    }
+    // 否则就更新数据或者什么都不做
+    else {
+        // 判断文档是否要更新（todo）
+        console.log(doc_id + "已经有数据了");
+      }
+  };
+
+  var splitArticle = React.useMemo(function () {
+    if (info.article) {
+      var sArr = info.article.split("。");
+      return sArr;
+    }
+    return [];
+  }, [info]);
+
+  // 写数据
+  utools.onPluginReady(function () {
+    // 获取数据
+    var dataArr = readConfig();
+    var dataObj = {
+      page1: dataArr.slice(0, 500),
+      page2: dataArr.slice(500, 1000),
+      page3: dataArr.slice(1000, 1500),
+      page4: dataArr.slice(1500, 2000),
+      page5: dataArr.slice(1500, 2000),
+      page6: dataArr.slice(2500, 3000),
+      page7: dataArr.slice(3000)
+    };
+    Object.keys(dataObj).forEach(function (item) {
+      initData(DOCID + "/oneword/" + item, dataObj[item]);
+    });
+  });
+
+  var getDaliyImg = function getDaliyImg() {
     // 获取每日图片
     fetch("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1").then(function (res) {
       return res.json();
@@ -20,40 +84,15 @@ var App = function App() {
       var url = "http://bing.com" + res.images[0].url;
       setImgUrl(url);
     });
+  };
+  React.useEffect(function () {
+    getDaliyImg();
 
-    // 写数据
+    var _utools$db$get = utools.db.get(DOCID + "/oneword/page1"),
+        page1Data = _utools$db$get.data;
 
-    utools.onPluginReady(function () {
-      var doc = utools.db.get(DOCID);
-      if (!doc) {
-        // 获取数据
-        var dataArr = readConfig();
-        console.log(dataArr);
-        // 创建
-
-        var _utools$db$put = utools.db.put({
-          _id: DOCID,
-          data: dataArr
-        }),
-            rev = _utools$db$put.rev,
-            ok = _utools$db$put.ok,
-            id = _utools$db$put.id;
-
-        if (ok) {
-          console.log("数据库创建成功, id为" + id);
-          console.log(rev);
-          // 存储rev
-          utools.dbStorage.setItem("eng_rev", rev);
-        } else {
-          console.log("数据库创建失败");
-        }
-      } else {
-        // 判断文档是否要更新（todo）
-        console.log("已经有数据了,你看");
-        var data = utools.get(DOCID);
-        console.log(data);
-      }
-    });
+    var todayWord = page1Data[3];
+    setInfo(todayWord);
   }, []);
 
   return React.createElement(
@@ -70,12 +109,18 @@ var App = function App() {
       React.createElement(
         "h1",
         null,
-        "pass out"
+        info.title
       ),
       React.createElement(
         "article",
         null,
-        "\u4ECA\u5929\u6211\u4EEC\u8981\u5B66\u7684\u8BCD\u662F pass out. To pass out \u610F\u601D\u662F\u5931\u53BB\u77E5\u89C9\u3002A pilot who allegedly passed out in the cockpit was arrested on suspicion of being drunk. \u4E00\u4E2A\u636E\u62A5\u9053\u5728\u9A7E\u9A76\u8231\u5185\u4E0D\u7701\u4EBA\u4E8B\u7684\u98DE\u884C\u5458\u56E0\u6D89\u5ACC\u9189\u9152\u88AB\u902E\u6355\u3002A Texas couple was arrested after they passed out in the car with drugs and their two young children inside. \u5F97\u514B\u8428\u65AF\u5DDE\u4E00\u7537\u4E00\u5973\u88AB\u8B66\u65B9\u902E\u6355\uFF0C\u53D1\u73B0\u4ED6\u4EEC\u65F6\uFF0C\u4ED6\u4EEC\u5DF2\u7ECF\u4E0D\u7701\u4EBA\u4E8B\uFF0C\u8F66\u91CC\u8FD8\u6709\u4ED6\u4EEC\u7684\u4E24\u4E2A\u5C0F\u5B69\u5B50\u548C\u6BD2\u54C1\u3002\u597D\u7684\uFF0C\u6211\u4EEC\u4ECA\u5929\u5B66\u4E60\u7684\u8BCD\u662F pass out, pass out, pass out\u2026"
+        splitArticle.map(function (item, index) {
+          return React.createElement(
+            "p",
+            { key: index },
+            item
+          );
+        })
       )
     )
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,67 @@
 import { DOCID } from "../config/index.js";
 const App = () => {
-  const [dbData, setDbdata] = React.useState({});
+  const [info, setInfo] = React.useState({});
   const [imgUrl, setImgUrl] = React.useState(``);
-  React.useEffect(() => {
+
+  const removeRevFromDBStorage = (DOCID) => {
+    const { ok } = utools.db.remove(DOCID);
+    if (ok) {
+      utools.dbStorage.removeItem("eng_rev");
+      console.log("删除了");
+    }
+  };
+
+  const initData = (doc_id, data) => {
+    const doc = utools.db.get(doc_id);
+    // 新用户就新建数据库
+    if (!doc) {
+      // 创建数据库
+      const { rev, ok, id } = utools.db.put({
+        _id: doc_id,
+        data,
+      });
+      if (ok) {
+        console.log("数据库创建成功, id为" + id);
+        // 存储rev
+        utools.dbStorage.setItem(doc_id + "_rev", rev);
+      } else {
+        console.log("数据库创建失败");
+      }
+    }
+    // 否则就更新数据或者什么都不做
+    else {
+      // 判断文档是否要更新（todo）
+      console.log(doc_id + "已经有数据了");
+    }
+  };
+
+  const splitArticle = React.useMemo(() => {
+    if (info.article) {
+      const sArr = info.article.split("。");
+      return sArr;
+    }
+    return []
+  }, [info]);
+
+  // 写数据
+  utools.onPluginReady(() => {
+    // 获取数据
+    const dataArr = readConfig();
+    const dataObj = {
+      page1: dataArr.slice(0, 500),
+      page2: dataArr.slice(500, 1000),
+      page3: dataArr.slice(1000, 1500),
+      page4: dataArr.slice(1500, 2000),
+      page5: dataArr.slice(1500, 2000),
+      page6: dataArr.slice(2500, 3000),
+      page7: dataArr.slice(3000),
+    };
+    Object.keys(dataObj).forEach((item) => {
+      initData(`${DOCID}/oneword/${item}`, dataObj[item]);
+    });
+  });
+
+  const getDaliyImg = () => {
     // 获取每日图片
     fetch("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
       .then((res) => {
@@ -12,35 +71,12 @@ const App = () => {
         const url = "http://bing.com" + res.images[0].url;
         setImgUrl(url);
       });
-
-    // 写数据
-
-    utools.onPluginReady(() => {
-      const doc = utools.db.get(DOCID);
-      if (!doc) {
-        // 获取数据
-        const dataArr = readConfig();
-        console.log(dataArr);
-        // 创建
-        const { rev, ok, id } = utools.db.put({
-          _id: DOCID,
-          data: dataArr,
-        });
-        if (ok) {
-          console.log("数据库创建成功, id为" + id);
-          console.log(rev);
-          // 存储rev
-          utools.dbStorage.setItem("eng_rev", rev);
-        } else {
-          console.log("数据库创建失败");
-        }
-      } else {
-        // 判断文档是否要更新（todo）
-        console.log("已经有数据了,你看");
-        const data = utools.get(DOCID);
-        console.log(data);
-      }
-    });
+  };
+  React.useEffect(() => {
+    getDaliyImg();
+    const { data: page1Data } = utools.db.get(DOCID + "/oneword/page1");
+    const todayWord = page1Data[3];
+    setInfo(todayWord);
   }, []);
 
   return (
@@ -49,15 +85,19 @@ const App = () => {
         <img src={imgUrl} />
       </div>
       <div className="content-wrapper">
-        <h1>pass out</h1>
+        <h1>{info.title}</h1>
         <article>
-          今天我们要学的词是 pass out. To pass out 意思是失去知觉。A pilot who
+          {/* 今天我们要学的词是 pass out. To pass out 意思是失去知觉。A pilot who
           allegedly passed out in the cockpit was arrested on suspicion of being
           drunk. 一个据报道在驾驶舱内不省人事的飞行员因涉嫌醉酒被逮捕。A Texas
           couple was arrested after they passed out in the car with drugs and
           their two young children inside.
           得克萨斯州一男一女被警方逮捕，发现他们时，他们已经不省人事，车里还有他们的两个小孩子和毒品。好的，我们今天学习的词是
-          pass out, pass out, pass out…
+          pass out, pass out, pass out… */}
+          {/* {splitArticle(info.article)} */}
+          {splitArticle.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
         </article>
       </div>
     </div>
